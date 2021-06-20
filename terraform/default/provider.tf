5d0ba6adb167a51fdcbd0ccbd022c9e8bac0da5c
p0tr3c <p0tr3c@protonmail.com>
2021-06-20 12:11:19 +0100

    chore: pin aws provider version

--- terraform/default/provider.tf
+++ terraform/default/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.44.0"
+    }
+  }
+}
+
 provider "aws" {
   assume_role {
     role_arn         = var.provider_role
