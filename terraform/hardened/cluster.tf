ae5f5467e05542aa0cf41458c8c3a1db591d7d87
p0tr3c <p0tr3c@protonmail.com>
2021-06-22 11:50:57 +0100

    feat: enable secret envolope encryption

--- terraform/hardened/cluster.tf
+++ terraform/hardened/cluster.tf
@@ -6,10 +6,58 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.cluster.cluster_id
 }
 
+data "aws_caller_identity" "current" {}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+resource "aws_kms_key" "eks" {
+  description             = "EKS secrets envolope key"
+  policy                  = <<EOF
+{
+    "Version": "2012-10-17",
+    "Id": "auto-eks",
+    "Statement": [
+        {
+          "Sid": "Enable IAM policies",
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+           },
+          "Action": "kms:*",
+          "Resource": "*"
+        },
+        {
+          "Sid": "Grant EKS service access to use the key",
+          "Effect": "Allow",
+          "Principal": {
+            "AWS": "*"
+          },
+          "Action": [
+            "kms:Encrypt",
+            "kms:Decrypt",
+            "kms:ReEncrypt*",
+            "kms:GenerateDataKey*",
+            "kms:CreateGrant",
+            "kms:DescribeKey"
+          ],
+          "Resource": "*",
+          "Condition": {
+            "StringLike": {
+              "kms:ViaService": [
+                  "eks.*.amazonaws.com"
+              ]
+            }
+          }
+        }
+    ]
+} 
+EOF
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
 }
 
 module "cluster" {
@@ -34,4 +82,11 @@ module "cluster" {
   ]
 
   cluster_enabled_log_types = var.cluster_enabled_log_types
+
+  cluster_encryption_config = [
+    {
+      provider_key_arn = aws_kms_key.eks.arn
+      resources        = ["secrets"]
+    }
+  ]
 }
