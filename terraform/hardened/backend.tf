cb54aa86aa6ec33ba93455c8ebe11f279ac2abd8
p0tr3c <p0tr3c@protonmail.com>
2021-06-25 16:11:41 +0100

    refactor: togle public ip mapping

--- terraform/hardened/backend.tf
+++ terraform/hardened/backend.tf
@@ -1,8 +1,14 @@
+# terraform {
+#   backend "local" {
+#     path = "terraform.tfstate"
+#   }
+# }
+
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
     # TODO: update with your own organization
-    organization = ""
+    organization = "snyk-cloud-config"
 
     workspaces {
       name = "eks-demo-deployments"
