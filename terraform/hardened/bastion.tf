6fd3e42e21db3e344ef39dd9c6344699df604d04
p0tr3c <p0tr3c@protonmail.com>
2021-06-25 15:41:40 +0100

    refactor: use cloudposse bastion module

--- terraform/hardened/bastion.tf
+++ terraform/hardened/bastion.tf
@@ -1,34 +1,14 @@
-data "aws_ami" "default" {
-  most_recent = "true"
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.*-x86_64-ebs"]
-  }
-  owners = ["amazon"]
-}
+module "ec2_bastion" {
+  source  = "cloudposse/ec2-bastion-server/aws"
+  version = "0.28.0"
 
-data "template_file" "bastion_user_data" {
-  count    = var.bastion_enabled ? 1 : 0
-  template = file("${path.module}/${var.bastion_user_data_template}")
+  enabled = true
 
-  vars = {
-    user_data   = join("\n", var.bastion_user_data)
-    ssm_enabled = var.bastion_ssm_enabled
-  }
-}
-
-resource "aws_instance" "bastion" {
-  count                       = var.bastion_enabled ? 1 : 0
-  ami                         = data.aws_ami.default.id
+  name                        = "bastion"
   instance_type               = var.bastion_instance_type
-  user_data                   = length(var.bastion_user_data_base64) > 0 ? var.bastion_user_data_base64 : data.template_file.bastion_user_data[0].rendered
-  vpc_security_group_ids      = [aws_security_group.all_worker_mgmt.id]
+  security_groups             = [aws_security_group.all_worker_mgmt.id]
+  subnets                     = module.vpc.public_subnets
+  user_data                   = var.bastion_user_data
+  vpc_id                      = module.vpc.vpc_id
   associate_public_ip_address = var.bastion_associate_public_ip_address
-  subnet_id                   = module.vpc.private_subnets[0]
-  monitoring                  = var.bastion_monitoring
-
-  root_block_device {
-    encrypted   = true
-    volume_size = var.bastion_root_block_device_volume_size
-  }
 }
