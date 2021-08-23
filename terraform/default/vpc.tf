9e417ae03e4c38c4135e77333e18e28d3cf34701
p0tr3c <p0tr3c@protonmail.com>
2021-08-23 09:08:37 +0000

    feat: update default with alb

--- terraform/default/vpc.tf
+++ terraform/default/vpc.tf
@@ -13,4 +13,15 @@ module "vpc" {
   single_nat_gateway     = var.single_nat_gateway
   enable_vpn_gateway     = var.enable_vpn_gateway
   one_nat_gateway_per_az = var.one_nat_gateway_per_az
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+
 }
