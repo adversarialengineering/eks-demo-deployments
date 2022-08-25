e7e53d3055dc5c7b3413b9d0f8f6b0137f0c8f05
p0tr3c <p0tr3c@protonmail.com>
2022-08-25 08:43:21 +0100

    feat: update the context with token

--- tests/service-account-auth/get-token.sh
+++ tests/service-account-auth/get-token.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-kubectl config set-credentials sa-user --token=$(kubectl get secret $1 -o jsonpath={.data.token} | base64 -d)
+SECRET_NAME=$(kubectl get serviceaccount github-integration -o jsonpath={.secrets[0].name})
+CLUSTER=$(kubectl config view -o jsonpath={.clusters[0].name})
+kubectl config set-credentials sa-user --token=$(kubectl get secret $SECRET_NAME -o jsonpath={.data.token} | base64 -d)
+kubectl config set-context disclosed/eks-demo --cluster=$CLUSTER --user=sa-user
+kubectl config use-context disclosed/eks-demo
