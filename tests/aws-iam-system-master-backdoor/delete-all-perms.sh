8ed383ce82a7155dcc793e7c6ead573b8e77e462
p0tr3c <p0tr3c@protonmail.com>
2022-10-15 09:27:16 +0100

    feat: prevent accidental deletes

--- tests/aws-iam-system-master-backdoor/delete-all-perms.sh
+++ tests/aws-iam-system-master-backdoor/delete-all-perms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl delete configmap -n kube-system aws-auth
-kubectl delete clusterroles.rbac.authorization.k8s.io --all
-kubectl -n kube-system delete roles --all
+kubectl --kubeconfig .kubeconfig delete configmap -n kube-system aws-auth
+kubectl --kubeconfig .kubeconfig delete clusterroles.rbac.authorization.k8s.io --all
+kubectl --kubeconfig .kubeconfig -n kube-system delete roles --all
